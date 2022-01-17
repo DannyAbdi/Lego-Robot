798522085bc517b6246aa4fbacadee390e2ce999
Lucas-est3 <84906458+Lucas-est3@users.noreply.github.com>
2022-01-17 18:42:53 +0000

    Add files via upload
    Worksheet 1 work

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Button.LEDPattern(4); // Start the LEDs flashing Green
+		Sound.beepSequenceUp();
+		LCD.clear();
+		LCD.drawString("HOLY", 2, 2);
+		Delay.msDelay(1000);
+		LCD.drawString("SMOKE",2,3);
+
+
+	}
+
+}
