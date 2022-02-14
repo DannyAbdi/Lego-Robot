1e9984f420a7fe3d50014e0d7d67051e04b18eba
heysamurott <55547591+heysamurott@users.noreply.github.com>
2022-02-14 15:24:55 +0000

    Update EmergencyStop.java

--- Behaviors/EmergencyStop.java
+++ Behaviors/EmergencyStop.java
@@ -1,11 +1,20 @@
 import lejos.hardware.Button;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
 
 public class EmergencyStop implements Behavior {
 	private MovePilot pilot;
-	public EmergencyStop(MovePilot pilot) {
+	private EV3TouchSensor ts;
+	private SampleProvider touch;
+	float[] touches = new float[1];
+	float isTouched = touches[0];
+	
+	public EmergencyStop(MovePilot pilot, EV3TouchSensor ts) {
 		this.pilot = pilot;
+		this.ts = ts;
+		touch = ts.getTouchMode();
 	}
 	
 	public void action() {
@@ -17,7 +26,15 @@ public class EmergencyStop implements Behavior {
 	}
 	
 	public boolean takeControl() {
+		touch.fetchSample(touches, 0);
+		
+		if (isTouched == 1) {
+			ts.close();
+			return true;
+		}
+		
 		Button.ESCAPE.waitForPressAndRelease();
+		
 		return true;
 	}
 }
