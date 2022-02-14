36fe0befab839e93d8801347ad25066571d1df2b
DannyAbdi <89345218+DannyAbdi@users.noreply.github.com>
2022-02-14 14:51:41 +0000

    Add files via upload

---
+++ Behaviors/Light.java
@@ -0,0 +1,49 @@
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.subsumption.Behavior;
+
+public class Light implements Behavior {
+	
+	private EV3ColorSensor cs;
+	private MovePilot pilot;
+	private SampleProvider light;
+	float[] lightLevel = new float[1];
+
+	
+	final static float FAST = 200f;
+	final static float SLOW = 100f;
+	
+	public Light(MovePilot pilot, EV3ColorSensor cs) {
+		
+		this.pilot = pilot;
+		this.cs = cs;
+		light = cs.getRedMode();
+	}
+	
+	public boolean takeControl() {
+		
+		light.fetchSample(lightLevel, 0);
+		float currentLight = lightLevel[0];
+		
+		if (currentLight < 0.5f) {
+			cs.close();
+			return true;
+		}
+		cs.close();
+		return false;	
+	}
+	
+	public void action() {
+		
+		if (takeControl()) {
+			pilot.setLinearSpeed(FAST);
+		} else {
+			pilot.setLinearSpeed(SLOW);
+		}
+	}
+
+	public void suppress() {
+	
+	}
+}
